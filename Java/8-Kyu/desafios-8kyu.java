ad7f72714cc216867b27af0b77abc256191a055d
SauloCouto <saulocouto1221@gmail.com>
2024-01-31 22:43:32 -0300

    news challenges solved in java

--- Java/8-Kyu/desafios-8kyu.java
+++ Java/8-Kyu/desafios-8kyu.java
@@ -2,6 +2,8 @@
 
 //Crie uma função chamada shortcut para remover as vogais minúsculas (a, e, i, o, u) em uma determinada string.
 
+import java.util.Arrays;
+
 public class Kata {
     public static String shortcut(String input) {
       String newInput = input.replaceAll("[aeiou]", "");
@@ -86,5 +88,118 @@ public class Multiples {
             arr[i] = base * (i + 1);
         }
         return arr;
+    }
+}
+
+//Gafanhoto - Verifique o fator
+
+//Esta função deve testar se factoré um fator de base. Retorne true se for um fator ou false se não for.
+public class Kata() {
+    public static boolean checkForFactor(int base, int factor) {
+        return base % factor == 0;
+    }
+}
+
+//Operador lógico "ou" (XOR) exclusivo
+
+//Em algumas linguagens de script como PHP, existe um operador lógico (por exemplo &&, ||, and, or, , etc.) chamado "Or Exclusivo" 
+//(daí o nome deste Kata). O exclusivo or avalia dois booleanos. Em seguida, ele retorna truese exatamente uma das duas expressões for true, 
+//false caso contrário. 
+public class XOR {
+    public static boolean xor(boolean a, boolean b) {
+        return a != b;
+    }
+}
+
+//Gafanhoto - Mensagem Personalizada
+
+//Crie uma função que forneça uma saudação personalizada. Esta função leva dois parâmetros: namee owner.
+public class Kata {
+    public static String greet(String name, String owner) {
+        return name.equals(owner) ? "Hello boss" : "Hello guest";
+    }
+}
+
+//Abrevie um nome de duas palavras
+
+//Escreva uma função para converter um nome em iniciais. Este kata utiliza estritamente duas palavras com um espaço entre elas.
+//A saída deve ser duas letras maiúsculas com um ponto separando-as.
+public class AbbreviateTwoWords {
+    public static String abbrevName(String name) {
+        String[] nameSplit = name.split("[\\s]");
+
+        Object[] firstLetter = Arrays.stream(nameSplit).map(str -> str.substring(0, 1).toUpperCase()).toArray();
+
+        return String.format("%s.%s", (String) firstLetter[0], (String) firstLetter[1]);
+    }
+}
+
+//Gafanhoto - Soma
+
+//Escreva um programa que encontre a soma de todos os números de 1 a num. O número sempre será um número inteiro positivo maior que 0. 
+//Sua função só precisa retornar o resultado, o que é mostrado entre parênteses no exemplo abaixo é como você chega a esse resultado e 
+//não faz parte dele, veja os exemplos de testes.
+public class GrassHopper {
+    public static int summation(int n) {
+        int result = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            result += i;
+        }
+        return result;
+    }
+}
+
+//Bin to Decimal
+
+//Complete a função que converte um número binário (fornecido como uma string) em um número decimal.
+public class Converter {
+
+    public static int binToDecimal(String input) {
+        //Reverse string to convert
+        StringBuilder inputReverse = new StringBuilder(input);
+        inputReverse.reverse();
+
+        String[] chars = inputReverse.toString().split("");
+
+        //Mapping bits
+        Map<Integer, String> mapBit = new HashMap<>();
+      
+        //Mapping first bit  
+        mapBit.put(1, chars[0]);
+
+        Integer decimal = 0;
+
+        if (mapBit.get(1).equals("1")) {
+            decimal += 1;
+        }
+
+        Integer multiplier = 2;
+        
+        //Mapping the rest of the bits 
+        for (int i = 1; i < chars.length; i++) {
+            mapBit.put(multiplier, chars[i]);
+
+            if (mapBit.get(multiplier).equals("1")) {
+                decimal += multiplier;
+            }
+
+            multiplier *= 2;
+        }
+
+        return decimal;
+    }
+}
+
+//Inverter valores
+
+//Dado um conjunto de números, retorne o inverso aditivo de cada um. Cada positivo torna-se negativo e os negativos tornam-se positivos.
+public class Kata {
+    public static int[] invert(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+          array[i] = array[i] * -1;
+        }
+        
+        return array;
     }
 }
